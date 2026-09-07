6bfdd3605a3e76fd6e80c5f2e0317466ee82e47d
fatihguzel <fatihguzeldev@gmail.com>
2026-09-08 02:03:18 +0300

    feat(core): parse bounded PE header prefixes

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,5 @@
 mod address;
+mod pe;
 
 pub use address::{FileOffset, GuestAddress32, ProgramCounter32, RelativeVirtualAddress};
+pub use pe::{PeHeaderError, PeHeaderPrefix, PeKind, parse_pe_header_prefix};
